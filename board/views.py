404357e3518fde0cb20317e2d15d772c42d929fc
Saher Adel <saheradel777@gmail.com>
2023-11-23 01:42:27 +0200

    templates and static files

--- board/views.py
+++ board/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Board
 # Create your views here.
+def BoardList(request):
+    boards = Board.objects.all()
+    return render(request , 'home.html', {'boards':boards})
